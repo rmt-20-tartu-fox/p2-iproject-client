54e024ec4aa0f6b7c66e6c98ac388130829a27dd
Erno <bimonugraraga@gmail.com>
2022-02-22 13:45:33 +0700

    menambahakn ragister

--- r6-tracker/src/views/LoginForm.vue
+++ r6-tracker/src/views/LoginForm.vue
@@ -1,20 +1,20 @@
 <template>
-  <form @submit.prevent="userLogin">
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
-    </div>
-    <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+  <div>
+    <form @submit.prevent="userLogin">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
+      </div>
+
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <small class="form-text text-muted">Doesn't Have An Account, please <a href="" @click.prevent="toRegister">Register</a></small>
+  </div>
 </template>
 
 <script>
@@ -42,6 +42,9 @@ export default {
         .catch((err) => {
           console.log(err)
         })
+    },
+    toRegister(){
+      this.$router.push({name: 'RegisterForm'})
     }
   }
 }
