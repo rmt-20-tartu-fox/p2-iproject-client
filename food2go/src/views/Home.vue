3f3241b3667f81fb69ac79d784531f7b6e0167a8
agyadnkr <agyad.dinakara@gmail.com>
2022-02-22 15:54:02 +0700

    Build: Install vue2-google-maps

--- food2go/src/views/Home.vue
+++ food2go/src/views/Home.vue
@@ -2,6 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+
+     <div class="w-full h-screen">
+    <gmap-map
+      :zoom="16"
+      :center="center"
+      :options="options"
+      style="width: 100%; height: 100%"
+    >
+      <gmap-marker
+        v-for="(loc, index) in locationMarkers"
+        :key="index"
+        :position="loc.position"
+        :clickable="true"
+        :draggable="false"
+        @click="center = loc.position"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
   </div>
 </template>
 
