dca67251de3f1a5b6b2069693f01a373d8405f2a
agyadnkr <agyad.dinakara@gmail.com>
2022-02-22 15:49:57 +0700

    Others: Setting google maps on main.js

--- food2go/src/main.js
+++ food2go/src/main.js
@@ -3,8 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import './assets/tailwind.css'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBjlQ0LbPIFIH3rExuCRIFoDifRyNAyenw",
+    libraries: "places"
+  }
+});
 
 new Vue({
   router,
