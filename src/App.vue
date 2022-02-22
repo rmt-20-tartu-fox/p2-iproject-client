91807cbb839fdf3a46ff90cd44668bcddb9d5134
AriefNugroho <arief.zhang21@gmail.com>
2022-02-22 12:15:47 +0700

    - Add Navbar - Add Carousel - Add Position Home Page :wq

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <div class="navbar-menu flex-row">
+      <nav class="navMenu">
+        <a href="#"> home </a>
+        <a href="#"> bookmark </a>
+        <a href="#"> login </a>
+        <a href="#"> logout </a>
+        <!-- <div class="dot"></div> -->
+      </nav>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+/* NAVBAR */
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap");
+
+.navbar-menu {
+  background-color: #272727;
+  font-family: "Montserrat", sans-serif;
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 10px;
+  width: 100%;
+  height: 60px;
+}
+
+.navMenu {
+  z-index: 1000;
+  display: flex;
+  max-width: 100%;
+  top: 50%;
+  left: 50%;
+}
+
+.navMenu a {
+  color: #f6f4e6;
+  text-decoration: none;
+  font-size: 1.2em;
+  text-transform: uppercase;
+  font-weight: 500;
+  display: inline-block;
+  width: 150px;
+  -webkit-transition: all 0.2s ease-in-out;
+  transition: all 0.2s ease-in-out;
+}
+
+.navMenu a:hover {
+  color: #fddb3a;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <!-- Carousel -->
+    <div
+      id="carouselExampleIndicators"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item">
+          <img
+            class="d-block w-100 cropped"
+            src="@/assets/images/carousel/bandung-ciwidey.jpg"
+            alt="First slide"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>SUNSET AT CIWIDEY</h1>
+            <p>BANDUNG DESTINATION HOLIDAY</p>
+          </div>
+        </div>
+        <div class="carousel-item active">
+          <img
+            class="d-block w-100 cropped"
+            src="@/assets/images/carousel/JawaTimur_Malang-JurassicPark.jpg"
+            alt="Second slide"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>JURASSIC PARK</h1>
+            <p>MALANG DESTINATION HOLIDAY</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100 cropped"
+            src="@/assets/images/carousel/NTT-labuan_bajo.jpg"
+            alt="Third slide"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>LABUAN BAJO</h1>
+            <p>NTT DESTINATION HOLIDAY</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100 cropped"
+            src="@/assets/images/carousel/bali-kuta.jpg"
+            alt="Four slide"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>KUTA BEACH</h1>
+            <p>BALI DESTINATION HOLIDAY</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100 cropped"
+            src="@/assets/images/carousel/SulUt-Bunaken.jpg"
+            alt="Five slide"
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h1>BUNAKEN</h1>
+            <p>MANADO DESTINATION HOLIDAY</p>
+          </div>
+        </div>
+        <ol class="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            class="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+    <!-- end Carousel -->
+    <div class="container-xl d-flex flex-col w-100" style="margin: 0 auto">
+      <div
+        class="container-fluid d-flex flex-col"
+        style="background-color: grey; height: 100vh; width: 30%"
+      ></div>
+      <div
+        class="container d-flex flex-col"
+        style="background-color: black; height: 100vh; width: 70%"
+      >
+      <!-- card -->
+
+      <!-- end card -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      active: false,
+    }
+  },
+ methods: {
+    carousel() {
+      this.active = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 900;
+  text-shadow: 3rem;
+}
+
+p {
+  font-weight: 400;
+}
+
+.cropped {
+  object-fit: cover;
+  object-position: 50% 40%;
+  height: 80vh;
+}
+</style>
