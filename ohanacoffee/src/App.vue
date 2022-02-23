6e60e92691535c16c8cf718496e9e5f0cbe180ff
qhalbym <qhalbym@gmail.com>
2022-02-23 18:24:11 +0700

    add face mask detection feature

---
+++ ohanacoffee/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <nav class="bg-white h-5">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+<style>
+</style>
