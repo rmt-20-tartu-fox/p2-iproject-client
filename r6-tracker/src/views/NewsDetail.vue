6307f1efdd68a159a949eb21dc56885797f76f22
Erno <bimonugraraga@gmail.com>
2022-02-23 22:15:24 +0700

    layouting

---
+++ r6-tracker/src/views/NewsDetail.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div>
+    <div v-if='news === null'>
+      <img src='http://i.imgur.com/aCK8eiA.gif'>
+    </div>
+    <div v-if='news !== null'>
+      <img v-bind:src="news.thumbnail.url" alt="">
+      <p style="color: white; background-color:grey; text-align: left;"><span v-html="news.content"></span></p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'News',
+  created(){
+    this.$store.dispatch('getOneNews', this.$route.params.id)
+  },
+  computed: {
+    news(){
+      return this.$store.state.news
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
