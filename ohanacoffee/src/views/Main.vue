5257cebd263e1e94b90c10206f3039a47403f9c3
qhalbym <qhalbym@gmail.com>
2022-02-24 06:05:20 +0700

    add cart page

--- ohanacoffee/src/views/Main.vue
+++ ohanacoffee/src/views/Main.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
-    <button @click.prevent="checkOut()"> checkout</button>
-    <div class="flex">
-      <div v-for="product in products" :key="product.id"  class="max-w-sm rounded mx-4 mt-4 overflow-hidden shadow-lg">
+    <div class="grid grid-cols-5">
+      <div>
+        <img width="100" src="../assets/banner.png" alt="">
+      </div>
+      <div class="flex col-span-3">
+        <label class="my-auto mx-4" for="search">Search:</label>
+        <input class="w-full border-2 h-10 my-auto" type="text">
+      </div>
+      <div class=" mx-4 my-auto text-right">
+        <button @click.prevent="checkOut()"><i class="fa-solid fa-cart-shopping fa-2xl"></i></button>
+      </div>
+    </div>
+
+    <div class="flex px-10">
+      <div v-for="product in products" :key="product.id" class="max-w-sm rounded mx-4 mt-4 overflow-hidden shadow-lg hover:shadow-indigo-500/40 hover:shadow-xl">
         <img @click="addToCart(product.id, product.price)" class="w-full" src="https://d1sag4ddilekf6.azureedge.net/compressed/items/6-CYLWVCBKAA33C2-CYLWVCBKGE6WGN/photo/menueditor_item_c21ac25fa7534072a2bb98dd967cb1fb_1584856720011716077.jpg" alt="Sunset in the mountains">
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{{product.name}}</div>
-          <div class="font-bold text-xl mb-2">{{product.price}}</div>
+          <div class="grid grid-cols-2">
+            <div class="font-bold text-xl mb-2">{{product.price}}</div>
+            <div v-if="cart.products[product.id]" class="font-bold text-xl mb-2">x {{cart.products[product.id].qty}}</div>
+            <button v-if="cart.products[product.id]" @click.prevent="decreaseQty(product.id)"><i class="fa-solid fa-circle-minus"></i></button>
+          </div>
         </div>
       </div>
     </div>
@@ -19,7 +35,7 @@
 import * as faceapi from "face-api.js";
 
 export default {
-  name: 'HomeView',
+  name: 'MainPage',
   components: {
   },
   data() {
@@ -29,10 +45,11 @@ export default {
       payButton: null,
       cart: {
         UserId: 4,
-        products: [],
+        products: {},
         CustomerId: null,
         total: 0
-      }
+      },
+      arrayProducts: []
     }
   },
   computed: {
@@ -43,6 +60,9 @@ export default {
   created() {
     this.$store.dispatch('fetchProducts')
     this.$store.dispatch('getBackground')
+    this.$store.state.products.forEach(e => {
+      this.selected[e.id] = {qty: 5}
+    });
   },
   mounted() {
     // this.videoEl = document.getElementById('videoEl')
@@ -73,7 +93,6 @@ export default {
           )
           .withFaceLandmarks()
           .withFaceDescriptor()
-          console.log(detection);
           if(detection) {
             if(detection.detection.score > 0.7) {
               this.useMask = false
@@ -86,19 +105,42 @@ export default {
       }, 2000);
     },
     addToCart(id, price) {
-      this.cart.products.push(id)
-      this.cart.total += price
+      if(this.cart.products[id]) {
+        const quantity = this.cart.products[id].qty + 1
+        this.cart.products[id] = {id, price, qty: quantity}
+      } else {
+        this.cart.products[id] = {id, price, qty: 1}
+      }
+      let convertProductToArray = Object.values(this.cart.products)
+      this.arrayProducts = convertProductToArray
+      this.$store.dispatch('fetchProducts')
+      console.log(this.cart.products);
     },
     checkOut() {
       this.createCart()
+      this.clearCart()
       this.$router.push({
-        name: "Cart",
-        params: this.cart
+        name: 'Cart',
+        params: {products: this.arrayProducts, cart: this.cart}
       })
     },
     createCart() {
       this.$store.dispatch('getTransactionToken', this.cart)
     },
+    clearCart() {
+      this.cart.UserId = null
+      this.cart.products = [],
+      this.cart.CustomerId = null,
+      this.cart.total = 0
+    },
+    decreaseQty(id) {
+      if(this.cart.products[id].qty < 1) {
+        this.cart.products[id] = null
+      } else {
+        this.cart.products[id].qty--
+      }
+      this.$store.dispatch('fetchProducts')
+    }
   }
 }
 </script>
