1208b149d329f11151f967ad69ea907f4403c664
aryawdy <aryawdy16@gmail.com>
2022-02-24 11:34:58 +0700

    setup deploy

--- apis/url.js
+++ apis/url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://afternoon-reef-54014.herokuapp.com/",
 });
 
 export default instance;
