fb4ead6f3b696a9522f6b464253c066767f44077
haryosmd <haryosmd@gmail.com>
2022-02-24 11:39:47 +0700

    deploy

--- src/apis/api_url.js
+++ src/apis/api_url.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // base Url
-const API_URL = "http://localhost:3000";
+const API_URL = "https://smd-movie-iproject.herokuapp.com";
 
 // Instance axios
 const instance = axios.create({
